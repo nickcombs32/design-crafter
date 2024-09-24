7b0c5a378487a15c877201633c5b1f022b56827b
nickcombs32 <31995688+nickcombs32@users.noreply.github.com>
2024-09-23 22:35:32 -0700

    Add files via upload
    - black surface
    - brush cursor

--- script.js
+++ script.js
@@ -4,12 +4,37 @@ space.height = window.innerHeight
 const ctx = space.getContext('2d')
 
 let surface = {
-    width: 900,
-    height: 600,
-    color: "white"
+  width: 900,
+  height: 600,
+  color: "black"
 }
 surface.pos = [space.width/2-surface.width/2,space.height/2-surface.height/2]
 
-ctx.fillStyle = surface.color
-ctx.fillRect(surface.pos[0],surface.pos[1],surface.width,surface.height)
+let pen = { 
+  radius: 3,
+  color: "white"
+}
+update()
 
+space.addEventListener('pointermove', function(e) {
+  pen.x = e.offsetX
+  pen.y = e.offsetY
+  
+  update()
+})
+
+function update() {
+  ctx.clearRect(0, 0, space.width, space.height)
+
+  ctx.fillStyle = "#4c4c4c"
+  ctx.fillRect(surface.pos[0]+3, surface.pos[1]+3, surface.width, surface.height)
+  ctx.fillStyle = surface.color
+  ctx.fillRect(surface.pos[0], surface.pos[1], surface.width, surface.height)
+
+  if( pen.x ) {
+    ctx.beginPath()
+    ctx.arc(pen.x, pen.y, pen.radius, 0, 2 * Math.PI, false)
+    ctx.fillStyle = pen.color
+    ctx.fill()
+  }
+}
